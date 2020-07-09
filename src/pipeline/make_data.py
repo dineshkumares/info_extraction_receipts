4c1c07d4601db1ccf442679a864721a785fff8d8
Udip Bohara <boharaudip@gmail.com>
2020-07-08 21:50:35 -0400

    fixed data splits

--- src/pipeline/make_data.py
+++ src/pipeline/make_data.py
@@ -5,6 +5,7 @@ import torch_geometric.data
 import networkx as nx
 import numpy as np
 import os 
+import random
 
 """
 make dataset for modeling as torch_geometric.data.Data:
@@ -18,13 +19,6 @@ make dataset for modeling as torch_geometric.data.Data:
 - face (LongTensor, optional) – Face adjacency matrix with shape [3, num_faces]. (default: None)
 
 """
-
-
-def sample_mask(idx, l):
-    """Create mask."""
-    mask = np.zeros(l)
-    mask[idx] = 1
-    return np.array(mask, dtype=np.bool)
 
 def from_networkx(G):
     """Converts a :obj:`networkx.Graph` or :obj:`networkx.DiGraph` to a
@@ -60,8 +54,6 @@ def from_networkx(G):
 
 
 
-
-
 def get_data():
     """
     returns data into a pygeometric Batch dataset format
@@ -70,19 +62,37 @@ def get_data():
     l=os.listdir(path)
     files=[x.split('.')[0] for x in l]
     files.sort()
-    files = files[1:]
+    all_files = files[1:]
 
     list_of_graphs = []
 
-    for file in files[:20]:
+    r"""to create train,test,val data"""
+    files = all_files.copy()
+    random.shuffle(files)
 
+    r"""Resulting in 500 receipts for training, 63 receipts for validation, and 63 for testing."""
+    training,testing,validating = files[:500],files[500:563],files[563:]
+
+
+    for file in all_files:
+    
         connect = Grapher(file)
         G,_,_ = connect.graph_formation()
-        df = connect.relative_distance()
+        df = connect.relative_distance() 
         individual_data = from_networkx(G)
 
-        feature_cols = ['xmin', 'ymin', 'xmax', 'ymax','rd_b','line_number', 'rd_r', 'rd_t', 'rd_l']
+        feature_cols = ['xmin', 'ymin', 'xmax', 'ymax','rd_b','line_number', 'rd_r', 'rd_t', 'rd_l',\
+                        'n_upper', 'n_alpha', 'n_spaces', 'n_numeric','n_special']
+        feature_cols = ['rd_b','line_number', 'rd_r', 'rd_t', 'rd_l',\
+                        'n_upper', 'n_alpha', 'n_spaces', 'n_numeric','n_special']
         features = torch.tensor(df[feature_cols].values.astype(np.float32))
+
+
+        for col in df.columns:
+            try:
+                df[col] = df[col].str.strip()
+            except AttributeError:
+                pass
 
         df['labels'] = df['labels'].fillna('undefined')
         df.loc[df['labels'] == 'company', 'num_labels'] = 1
@@ -92,13 +102,35 @@ def get_data():
         df.loc[df['labels'] == 'total', 'num_labels'] = 5
         df.loc[df['labels'] == 'undefined', 'num_labels'] = 6
  
-        labels = torch.tensor(df['num_labels'].values.astype(np.int))
 
+        assert df['num_labels'].isnull().values.any() == False, f'labeling error! Invalid label(s) present in {file}.csv'
+
+
+        labels = torch.tensor(df['num_labels'].values.astype(np.int))
+    
         individual_data.x = features
         individual_data.y = labels
+        
 
-        print(individual_data)
+        r"""Create masks"""
+        if file in training:
+            individual_data.train_mask = torch.tensor([True] * df.shape[0])
+            individual_data.val_mask = torch.tensor([False] * df.shape[0])
+            individual_data.test_mask = torch.tensor([False] * df.shape[0])
 
+        elif file in validating:
+            individual_data.train_mask = torch.tensor([False] * df.shape[0])
+            individual_data.val_mask = torch.tensor([True] * df.shape[0])
+            individual_data.test_mask = torch.tensor([False] * df.shape[0])
+        else:
+            individual_data.train_mask = torch.tensor([False] * df.shape[0])
+            individual_data.val_mask = torch.tensor([False] * df.shape[0])
+            individual_data.test_mask = torch.tensor([True] * df.shape[0])
+
+
+
+
+        print(f'{file} ---> Success')
         list_of_graphs.append(individual_data)
     
 
@@ -107,60 +139,14 @@ def get_data():
 
     data.edge_attr = None 
 
-    #50,20,30 split for train,val,split
-    t = data.entity
-    entity_indices = np.where(t)[0].tolist()
-    non_entity_indices = np.where(t == False)[0].tolist()
-
-
-    #shuffle
-    np.random.shuffle(entity_indices)
-    np.random.shuffle(non_entity_indices)
-
-    """
-    50,20,30 split for train,val,split
-    Since the data is imabalanced (many 'undefined' classes),
-    I am ensuring that there is equal proportion of entities 
-    and non entities in all the splits 50-20-30.
-
-    """
-    num_entities = len(entity_indices) 
-
-
-    training_entities= int(50/100 * num_entities) 
-    validation_entities  = int(20/100 * num_entities )
-    testing_entities = num_entities - (training_entities + validation_entities)
-
-
-    num_non_entities = len(non_entity_indices)
-
-    training_non_entities= int(50/100 * num_non_entities) 
-    validation_non_entities  = int(20/100 * num_non_entities )
-    testing_non_entities = num_non_entities - (training_non_entities + validation_non_entities)
-
-
-    print(training_entities)
-
-
-    idx_train = entity_indices[:training_entities] + non_entity_indices[:training_non_entities]
-    idx_val = entity_indices[training_entities:validation_entities+training_entities] + \
-            non_entity_indices[training_non_entities:validation_non_entities+training_non_entities]
-
-    idx_test = entity_indices[-testing_entities:] + non_entity_indices[-testing_non_entities:]
-
-
-    data.train_mask = sample_mask(idx_train, data.y.shape[0])
-    data.val_mask = sample_mask(idx_val, data.y.shape[0])
-    data.test_mask = sample_mask(idx_test, data.y.shape[0])
-
     save_path = "../../data/processed/"  
-    torch.save(data, save_path +'data.dataset')
-
+    torch.save(data, save_path +'data2.dataset')
+    print('Data is saved!')
 
 
 if __name__ == "__main__":
-    #get_data()
-    data_path = "../../data/processed/" + "data.dataset"
+    get_data()
+    data_path = "../../data/processed/" + "data2.dataset"
     data = torch.load(data_path)
     print(data)
     print(data.__dict__)
