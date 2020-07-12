54cdb0267e06482b124af2f492f0aab2d2c5204e
Udip Bohara <boharaudip@gmail.com>
2020-07-12 18:20:16 -0400

    edits

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -19,14 +19,13 @@ raw_task2_files = list(
 
 jpg_files = [f for f in raw_task1_files if f.name.endswith("jpg")]
 csv_files = [f for f in raw_task1_files if f.name.endswith("txt")]
-json_files = [f for f in raw_task2_files if f.name.endswith("txt")]
 
-for i, (f_jpg, f_csv, f_json) in enumerate(zip(jpg_files, csv_files, json_files)):
+
+for i, (f_jpg, f_csv) in enumerate(zip(jpg_files, csv_files)):
     if noext(f_jpg) != noext(f_csv) or noext(f_csv) != noext(f_json):
         raise ValueError("Raw data filenames mismatch")
 
-    print(f"{i:03d}", f_jpg, f_csv, f_json)
+    print(f"{i:03d}", f_jpg, f_csv)
 
     replace(f_jpg.path, f"data/raw/img{i:03d}.jpg")
     replace(f_csv.path, f"data/raw/box{i:03d}.csv")
-    replace(f_json.path, f"data/raw/key{i:03d}.json")
