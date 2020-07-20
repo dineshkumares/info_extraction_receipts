e1046f961fc2ea084d10a364caf4ec72a5c2ddf4
Udip Bohara <49792339+udipbohara@users.noreply.github.com>
2020-07-20 09:47:08 -0400

    Update final_model.py

--- src/models/final_model.py
+++ src/models/final_model.py
@@ -18,15 +18,6 @@ print(f'training nodes: {data.y[data.train_mask].shape}')
 print(f'validation nodes: {data.y[data.val_mask].shape}')
 print(f'testing nodes: {data.y[data.test_mask].shape}')
 
-# print(data.y.unique())
-# print(data.y.shape)
-# print(data.y[data.train_mask].shape)
-# print(data.y.unique())
-# print(data.y)
-
-# unique_elements, counts_elements = np.unique(data.y, return_counts=True)
-# print(np.asarray((unique_elements, counts_elements)))
-
 # Training settings
 parser = argparse.ArgumentParser()
 
@@ -42,17 +33,12 @@ parser.add_argument('--weight_decay', type=float, default=5e-4,
                     help='Weight decay (L2 loss on parameters).')
 parser.add_argument('--early_stopping', type=int, default = 50,
                     help = 'Stopping criteria for validation')
-
-
-
 parser.add_argument('--use_gdc', action='store_true',
                     help='Use GDC preprocessing.')
 args = parser.parse_args()
 
 
 print(f'number of nodes: {data.x.shape}')
-
-
 
 class Net(torch.nn.Module):
     def __init__(self):
@@ -83,12 +69,7 @@ class Net(torch.nn.Module):
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model, data = Net().to(device), data.to(device)
-
-
-
 optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
-
-
 
 def train():
     model.train()
@@ -100,12 +81,8 @@ def train():
     class_weights = class_weight.compute_class_weight('balanced',
                                                  data.y.unique().numpy() ,
                                                  data.y.numpy() )
-
-
-    #FIX WEIGHTS
+  
     weights = torch.FloatTensor(class_weights)
-    
-
     #weights = torch.tensor([8.1577,3.3419, 9.3718, 8.9813, 8.9526, 0.1905])
     
     loss = F.nll_loss(model()[data.train_mask], data.y[data.train_mask]-1, weight=weights)
@@ -113,18 +90,14 @@ def train():
     optimizer.step()
     return loss
 
-
 @torch.no_grad()
 def test():
     model.eval()
-    
     #F.nll_loss(model()[data.val_mask], data.y[data.val_mask])
     logits, accs = model(), []
     for mask_name, mask in data('train_mask', 'val_mask', 'test_mask'):
         pred = logits[mask].max(1)[1]
-
         acc = pred.eq(data.y[mask]-1).sum().item() / mask.sum().item()  
-
         if args.verbose == 1:
             # r"printing predicted classes and number of elements for preds"
             # print('pred')
@@ -135,40 +108,25 @@ def test():
             # print('actual')
             # unique_elements, counts_elements = np.unique((data.y[mask]-1), return_counts=True)
             # print(np.asarray((unique_elements, counts_elements)))
- 
-
         #confusion matrix
             if mask_name == 'test_mask':
                 conf_mat=confusion_matrix((data.y[mask]-1).numpy(), pred.numpy())
                 print(f'confusion_matrix: \n   {conf_mat}')
                 class_accuracy=100*conf_mat.diagonal()/conf_mat.sum(1)
                 print(class_accuracy)
-
-       # print(acc)
         accs.append(acc)
-
     return accs
-
-
+  
 if __name__ == '__main__':
 #stopping criteria 
     counter = 0
-
-
     for epoch in range(1, args.epochs+1):
-        
-
         loss = train()
         train_acc, val_acc, test_acc = test()
-
         with torch.no_grad():
             #print(model()[data.val_mask])
-            
             loss_val = F.nll_loss(model()[data.val_mask], data.y[data.val_mask]-1)
             #print(model()[data.val_mask])
-        
-    
-        
         log = 'Epoch: {:03d}, train_loss:{:.4f}, val_loss:{:.4f}, Train: {:.4f}, Val: {:.4f}, Test: {:.4f}'
         print(log.format(epoch,loss,loss_val, train_acc, val_acc, test_acc))
 
